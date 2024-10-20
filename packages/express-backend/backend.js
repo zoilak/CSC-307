a206c128c4112bee65427a5ebf0796bc8d7cc817
zkanu <zoilanurikanu@gmail.com>
2024-10-19 20:07:16 -0700

    Added  user/id endpoints

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -34,6 +34,15 @@ const users = {
     ]
   };
 
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name
+    );
+};
+ 
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+
 app.use(express.json()); //et up our express app to process incoming data in JSON format. With that, Express (as a middleware) will allow us to access JSON data seamlessly in memory.
 
 app.get("/", (req, res) => { //first API endpoint; / is the URL pattern that will map to the function
@@ -41,7 +50,26 @@ app.get("/", (req, res) => { //first API endpoint; / is the URL pattern that wil
 });
 
 app.get("/users", (req, res) => {
+//     res.send(users);
+//   });
+    const name = req.query.name;
+    if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+    } else {
     res.send(users);
+    }
+});
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+      res.status(404).send("Resource not found.");
+    } else {
+      res.send(result);
+    }
   });
 
 
